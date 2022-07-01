4f511146a92695fb9049b86e7c4a7a94a2c39d39
Drako9159 <chochito9159@gmail.com>
2022-07-01 14:56:24 -0500

    add utiliti javascript

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+const moreOptions = document.querySelector("#bmore");
+const bShowMobileLinks = document.querySelector("#bmenu");
+const mobileMenu = document.querySelector(".links");
+const moreMenu = document.querySelector(".more .menu");
+
