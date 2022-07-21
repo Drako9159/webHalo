442f48b727eed228e4b42a1525c8088cd124f691
drako9159 <chochito9159@gmail.com>
2022-07-21 12:38:46 -0500

    add basic serve

---
+++ src/js/server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+function sitioweb(req, res){
+    res.writeHead(200, {"Content-type": "text/plain"} );
+    res.end("running server");
+}
+let servidor = http.createServer(sitioweb);
+servidor.listen(8081, "192.168.1.140");
+
+console.log("server runing");
