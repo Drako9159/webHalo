fb11c885afd89fa027db2dbf49c457ec3c9c2c76
drako9159 <chochito9159@gmail.com>
2022-07-21 19:42:18 -0500

    add consult api to nationalize

---
+++ src/js/consultApi.js
@@ -0,0 +1,32 @@
+import fetch from "node-fetch";
+
+async function comeMade(name){
+    const url = `https://api.nationalize.io/?name=${name}`;
+     await fetch(url).then((res) => {
+        if(res.status != "200"){
+            let none = "404 no se encontró";
+            throw new Error(none);
+        } else {
+            return res.json();
+        }
+    }).then((data) => {
+        console.log(data);
+    })
+}
+console.log(comeMade("sofia"));
+
+/*async function comeMade(){
+    const response = await fetch("https://api.nationalize.io/?name=antonio");
+    
+    if(!response.Ok){
+        let none = "404 no se encontró";
+        throw new Error(none);
+    }
+    const name = response.json();
+    return name;
+}
+function showName(n){
+    console.log(n)
+}
+comeMade().then(showName);
+*/
